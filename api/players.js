ad08b4b14dbc8e1674e6148941e6cc1515b8eb84
Zecay <110484412+Zecay@users.noreply.github.com>
2026-02-08 16:52:25 +0100

    Update players.js

--- api/players.js
+++ api/players.js
@@ -7,10 +7,17 @@ export default async function handler(req, res) {
         );
         const data = await r.json();
 
-        res.status(200).json({
-            players: data.data[0].playing
-        });
-    } catch {
+        // ✅ Add CORS headers
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Methods", "GET");
+        res.setHeader("Content-Type", "application/json");
+
+        res.status(200).json({ players: data.data[0].playing });
+    } catch (error) {
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Methods", "GET");
+        res.setHeader("Content-Type", "application/json");
+
         res.status(500).json({ players: 0 });
     }
 }
